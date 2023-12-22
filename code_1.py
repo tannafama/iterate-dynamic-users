f7bb3a982224c0dc68e83aaf4794c4109286e14e
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-12-22 17:45:19 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,58 @@
+    }
+    const data = [];
+main();
+}
+
+    return data;
+function generateRandomData() {
+    });
+}
+
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+function main() {
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    for item in data:
+if __name__ == "__main__":
+import random
+        print(f"Random Number: {item}")
+def generate_random_data():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    return data
+
+def main():    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+        console.log("Random Number: " + item);
+function main() {
+    });
+    const data = [];
+function generateRandomData() {
+}
+
+    return data;
+    data.forEach(item => {
+    const data = generateRandomData();
+main();
+    }
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+    data = generate_random_data()
+    for item in data:
+def generate_random_data():
+
+    main()
+if __name__ == "__main__":
+
+    return data
